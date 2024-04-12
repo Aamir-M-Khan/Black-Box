68d56f385dec9fa3d2567256c78480d044901537
Aamir-M-Khan <aamir11amu@gmail.com>
2024-04-12 12:00:50 +0200

    new parameters added

--- black-box/tensor.py
+++ black-box/tensor.py
@@ -1,8 +1,12 @@
 import numpy as np
 
 class Tensor:
-    def __init__(self, value):
+    def __init__(self, value, creators, operator, autograd):
         self.value = np.array(value)
+        self.creators = creators
+        self.operator = operator
+        self.autograd = autograd
+
 
     def __str__(self):
         return str(self.value)
@@ -11,12 +15,27 @@ class Tensor:
         return str(self.value.__repr__())
     
     def __add__(self, other):
+        if self.autograd & other.autograd:
+            return Tensor(self.value + other.value,
+                        creators=[self, other],
+                        operator = "add",
+                        autograd = True)
         return Tensor(self.value + other.value)
-    
+        
     def __sub__(self, other):
+        if self.autograd & other.autograd:
+            return Tensor(self.value - other.value,
+                        creators=[self, other],
+                        operator = "sub",
+                        autograd = True)            
         return Tensor(self.value - other.value)
     
     def __mul__(self, other):
+        if self.autograd & other.autograd:
+            return Tensor(self.value * other.value,
+                        creators=[self, other],
+                        operator = "mul",
+                        autograd = True)
         return Tensor(self.value * other.value)
     
-    
+
