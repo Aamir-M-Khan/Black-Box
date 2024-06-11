32d1523d672f258ddc140ea6a9525128266c0c5b
Aamir-M-Khan <aamir11amu@gmail.com>
2024-06-11 23:12:41 +0200

    conv2d is added

--- black-box/functional.py
+++ black-box/functional.py
@@ -67,4 +67,37 @@ def transposed_conv1d(signal, kernel, stride=1, padding=0):
 
     return result
 
-      
+def conv2d(image, kernel, padding=0):
+    """
+    Perform 2D convolution on an image using a kernel with padding,
+
+    Arguments:
+    image -- 2D numpy array, representing the input image
+    kernel -- 2D numpy array, representing the convolutional kernel.
+    padding -- integer, amount of padding to add around the image.
+
+    Returns:
+    result -- 2D numpy array, result of the convolution.
+    
+    """
+    # Get the dimension of the image and the kernel
+    image_height, image_width = image.shape
+    kernel_height, kernel_width = kernel.shape
+
+    # Calculate the output dimensions after convolution with padding
+    output_height = image_height - kernel_height + 1 + 2 * padding
+    output_width = image_width - kernel_width + 1 + 2*padding
+
+    # Initialize the result after convolution
+    result_conv = np.zeros(output_height, output_width)
+
+    # Add padding to the image
+    padded_image = np.pad(image, pad_width=padding, mode='constant', constant_values=0)
+
+    # Perform the convolution
+    for y in range(output_height):
+        for x in range(output_width):
+            result_conv[y, x] = np.sum(padded_image[y:y+kernel_height, x:x+kernel_width]*kernel)
+
+    return result_conv
+
